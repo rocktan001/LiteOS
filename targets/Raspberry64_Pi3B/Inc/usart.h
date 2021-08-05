a83436c5fac7d18be1112719f5fa1ae52b17264f
baochenjin <760281102@qq.com>
2021-08-05 03:55:08 +0000

    !235 add board Raspberry64_Pi3B without smp * add board Raspberry64_Pi3B without smp

---
+++ targets/Raspberry64_Pi3B/Inc/usart.h
@@ -0,0 +1,100 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ * Description: Usart HeadFile
+ * Author: Huawei LiteOS Team
+ * Create: 2021-08-04
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific prior written
+ * permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --------------------------------------------------------------------------- */
+
+#ifndef _USART_H
+#define _USART_H
+
+#include "los_typedef.h"
+#include "uart.h"
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+#endif /* __cplusplus */
+
+#define UART_RXREADY_FLAG   0x1
+#define UART_TXEMPTY_FLAG   0x20
+
+#define AUX_BASE        (0x3F000000)
+#define AUX_REG_BASE    (AUX_BASE + 0x215000)
+#define AUX_ENABLES     (AUX_REG_BASE + 0x4)
+#define MINI_UART       ((MINIUART_INFO *)(AUX_REG_BASE + 0x40))
+#define GPIO_REG_BASE   ((GPIO_INFO *)(AUX_BASE + 0x200000))
+
+typedef struct tagGpioInfo {
+    volatile UINT32 GPFSEL[6];      /* GPIO Function Select x */
+    volatile UINT32 RES0;           /* Reserved */
+    volatile UINT32 GPSET[2];       /* GPIO Pin Output Set x */
+    volatile UINT32 RES1;           /* Reserved */
+    volatile UINT32 GPCLR[2];       /* GPIO Pin Output Clear x */
+    volatile UINT32 RES2;           /* Reserved */
+    volatile UINT32 GPLEV[2];       /* GPIO Pin Level x */
+    volatile UINT32 RES3;           /* Reserved */
+    volatile UINT32 GPEDS[2];       /* GPIO Pin Event Detect Status x */
+    volatile UINT32 RES4;           /* Reserved */
+    volatile UINT32 GPREN[2];       /* GPIO Pin Rising Edge Detect Enable x */
+    volatile UINT32 RES5;           /* Reserved */
+    volatile UINT32 GPFEN[2];       /* GPIO Pin Falling Edge Detect Enable x */
+    volatile UINT32 RES6;           /* Reserved */
+    volatile UINT32 GPHEN[2];       /* GPIO Pin High Detect Enable x */
+    volatile UINT32 RES7;           /* Reserved */
+    volatile UINT32 GPLEN[2];       /* GPIO Pin Low Detect Enable 0 */
+    volatile UINT32 RES8;           /* Reserved */
+    volatile UINT32 GPAREN[2];      /* GPIO Pin Async. Rising Edge Detect x */
+    volatile UINT32 RES9;           /* Reserved */
+    volatile UINT32 GPAFEN[2];      /* GPIO Pin Async. Falling Edge Detect x */
+    volatile UINT32 RES10;          /* Reserved */
+    volatile UINT32 GPPUD;          /* GPIO Pin Pull-up/down Enable */
+    volatile UINT32 GPPUDCLK[2];    /* GPIO Pin Pull-up/down Enable Clock x */
+} GPIO_INFO;
+
+typedef struct tagMiniUartInfo {
+    volatile UINT32 IO;         /* Mini Uart I/O Data 8 */
+    volatile UINT32 IIR;        /* Mini Uart Interrupt Identify */
+    volatile UINT32 IER;        /* Mini Uart Interrupt Enable */
+    volatile UINT32 LCR;        /* Mini Uart Line Control */
+    volatile UINT32 MCR;        /* Mini Uart Modem Control */
+    volatile UINT32 LSR;        /* Mini Uart Line Status */
+    volatile UINT32 MSR;        /* Mini Uart Modem Status */
+    volatile UINT32 SCRATCH;    /* Mini Uart Scratch */
+    volatile UINT32 CNTL;       /* Mini Uart Extra Control */
+    volatile UINT32 STAT;       /* Mini Uart Extra Status */
+    volatile UINT32 BAUD;       /* Mini Uart Baudrate */
+} MINIUART_INFO;
+
+extern UartControllerOps g_armGenericUart;
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* __cplusplus */
+
+#endif /* _USART_H */
+
---
+++ targets/Raspberry64_Pi3B/include/asm/interrupt_config.h
@@ -0,0 +1,110 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ * Description: Interrupt Config HeadFile
+ * Author: Huawei LiteOS Team
+ * Create: 2021-08-04
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific prior written
+ * permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --------------------------------------------------------------------------- */
+
+#ifndef _ASM_INTERRUPT_CONFIG_H
+#define _ASM_INTERRUPT_CONFIG_H
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+#endif /* __cplusplus */
+
+#define ARM_TIMER_INI               0
+#define AUX_INT                     29
+#define I2C_SPI_SLV_INT             43
+#define PWA0_INT                    45
+#define PWA1_INT                    46
+#define CNTPS_IRQ                   65
+#define CNTPNS_IRQ                  66
+#define CNTHP_IRQ                   67
+#define CNTV_IRQ                    68
+#define MAILBOX0_IRQ                69
+#define MAILBOX1_IRQ                70
+#define MAILBOX2_IRQ                71
+#define MAILBOX3_IRQ                72
+#ifdef LOSCFG_KERNEL_SMP
+#define OS_TICK_INT_NUM             CNTPNS_IRQ
+#else
+#define OS_TICK_INT_NUM             ARM_TIMER_INI
+#endif
+
+#define RASPBERRY_IRQ               MAILBOX3_IRQ
+#define NUM_HAL_INTERRUPT_UART      AUX_INT
+
+#define IRQ_REG_BASE                ((INTERRUPTS_INFO *)(0x3F000000 + 0xB200))
+#define SYSTEMTIMER_REG_BASE        ((SYSTEMTIMER_INFO *)(0x3F000000 + 0x3000))
+#define ARMTIMER_REG_BASE           ((ARMTIMER_INFO *)(0x3F000000 + 0xB000 + 0x400))
+#define CORE_MAILBOX_REG_BASE       ((MAILBOXES_INFO *)(0x40000000 + 0x40))
+
+typedef struct tagInterruptsInfo {
+    volatile UINT32 basicPending;       /* IRQ basic pending */
+    volatile UINT32 pending[2];         /* IRQ pending x */
+    volatile UINT32 FIQControl;         /* FIQ control */
+    volatile UINT32 enableIRQs[2];      /* Enable IRQs */
+    volatile UINT32 enableBasicIRQs;    /* Enable Basic IRQs */
+    volatile UINT32 disableIRQs[2];     /* Disable IRQs x */
+    volatile UINT32 disableBasicIRQs;   /* Disable Basic IRQs */
+} INTERRUPTS_INFO;
+
+typedef struct tagArmTimerInfo {
+    volatile UINT32 load;
+    volatile UINT32 value;
+    volatile UINT32 control;
+    volatile UINT32 IRQClear;
+    volatile UINT32 rawIRQ;
+    volatile UINT32 maskedIRQ;
+    volatile UINT32 reload;
+    volatile UINT32 preDivider;
+    volatile UINT32 freeRunningCounter;
+} ARMTIMER_INFO;
+
+typedef struct tagSystemTimerInfo {
+    volatile UINT32 CS;         /* System Timer Control/Status  */
+    volatile UINT32 CLO;        /* System Timer Counter Lower 32 bits */
+    volatile UINT32 CHI;        /* System Timer Counter Higher 32 bits */
+    volatile UINT32 C[4];       /* System Timer Compare x */
+} SYSTEMTIMER_INFO;
+
+typedef struct tagMailboxesInfo {
+    volatile UINT32 CoreTimeIRQ[4]; /* Core timers interrupts */
+    volatile UINT32 IRQControl[4];  /* Core x timers Interrupt control */
+    volatile UINT32 IRQSource[4];   /* Core x IRQ Source*/
+    volatile UINT32 FIQSource[4];   /* Core x FIQ Source*/
+    volatile UINT32 writeSet[16];   /* Core x Mailbox x write-set */
+    volatile UINT32 readClear[16];  /* Core x Mailbox x read & write-high-to-clear */
+} MAILBOXES_INFO;
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* __cplusplus */
+
+#endif /* _ASM_INTERRUPT_CONFIG_H */
+
